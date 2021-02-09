5c726483d482b80ffc2b6b61014ead2dd413f803
Jared Parrish <jared.parrish@alaska.gov>
2021-02-09 10:07:27 -0900

    Old - now setdata_1

--- R/setdata.R
+++ R/setdata.R
@@ -20,31 +20,34 @@ dat1$OnsetDate <- as.Date(as.POSIXct(dat1$OnsetDate/1000, origin = "1970-01-01",
   
 ### End Temp code  
   
-### Onset date imputation  
-# calcuate duration in days.
-dat1$ReportDate <- as.Date(with(dat1, ifelse(OnsetDateReplaced == 2, dat1$OnsetDate,
-                                             NA)), origin = "1970-01-01")
-dat1$duration <- dat1$ReportDate - dat1$OnsetDate
+## Onset date imputation
+#calcuate duration in days.
+#  dat1$ReportDate <- as.Date(with(dat1, ifelse(OnsetDateReplaced == 1, dat1$OnsetDate,
+#                                               NA)), origin = "1970-01-01")
+#  dat1$duration <- dat1$ReportDate - dat1$OnsetDate
+# #
+# #
+# # ## set distribution for median among most recent 14 days duration for Gamma dist
+# # ## had to modify and hard code this due to change in data structure, and assume it is constant.
+# set.seed(2020)
+# #dist <- round(rgamma(1000, shape =  sts$Y[3]))
+# #use a truncated gamma distribution to ensure that the selected durations are consistant
+# # with observed data and in a 2-week range and modify shape from 7 to 6.
+# 
+# dist <- round(rtgamma(50000, shape = 3, truncation = 14))
+# 
+# #zro <- table(dat1$duration<=0)[2] # isolate duration = 0 cases and impute values
+# zro <- table(dat1$duration)
+# set.seed(19)
+# smp1 <- sample(dist, zro)
+# dat1$duration_imp <- ifelse(dat1$duration == 0, smp1, dat1$duration)
+# 
+# dat1$OnsetDate1 <- as.Date(ifelse(is.na(dat1$duration), dat1$OnsetDate,
+#                                   dat1$OnsetDate - dat1$duration_imp),
+#                            origin = "1970-01-01")
 
+dat1$OnsetDate1 <- dat1$OnsetDate
 
-## set distribution for median among most recent 14 days duration for Gamma dist
-## had to modify and hard code this due to change in data structure, and assume it is constant.
-set.seed(2020)
-#dist <- round(rgamma(1000, shape =  sts$Y[3])) 
-#use a truncated gamma distribution to ensure that the selected durations are consistant
-# with observed data and in a 2-week range and modify shape from 7 to 6.
-
-dist <- round(rtgamma(50000, shape =  6, truncation = 14)) 
-
-#zro <- table(dat1$duration<=0)[2] # isolate duration = 0 cases and impute values
-zro <- table(dat1$duration)
-set.seed(19) 
-smp1 <- sample(dist, zro)
-dat1$duration_imp <- ifelse(dat1$duration == 0, smp1, dat1$duration)
-
-dat1$OnsetDate1 <- as.Date(ifelse(is.na(dat1$duration), dat1$OnsetDate,
-                                  dat1$OnsetDate - dat1$duration_imp),
-                           origin = "1970-01-01")
 
 # type of travel
 dat1$type <- with(dat1, ifelse(SpecificAcquisition %in% c("Community","Secondary"), "local",
